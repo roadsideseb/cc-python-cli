a8a80cadf21111dfb9426b109f1835467b21c004
Sebastian Vetter <seb@roadsi.de>
2018-06-19 13:55:51 -0600

    Use black formatting

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -3,9 +3,9 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-requires = ['click']
-tests_requires = ['pytest', 'pytest-cache', 'pytest-cov']
-lint_requires = ['flake8', 'black']
+requires = ["click"]
+tests_requires = ["pytest", "pytest-cache", "pytest-cov"]
+lint_requires = ["flake8", "black"]
 dev_requires = requires + tests_requires + lint_requires
 
 
@@ -18,31 +18,33 @@ class PyTest(TestCommand):
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
 
 setup(
     name="{{cookiecutter.repo_name}}",
-    version='0.0.0',
+    version="00.0.0.0",
     description="{{cookiecutter.description}}",
     long_description="\n\n".join([open("README.rst").read()]),
-    license='MIT',
+    license="MIT",
     author="{{cookiecutter.author_name}}",
     author_email="{{cookiecutter.author_email}}",
     url="https://{{cookiecutter.repo_name}}.readthedocs.org",
     packages=find_packages(),
     install_requires=requires,
-    entry_points={'console_scripts': [
-        '{{cookiecutter.cli_name}} = {{cookiecutter.package_name}}.cli:main']},
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython'],
-    extras_require={
-        'test': tests_requires,
-        'dev': dev_requires,
-        'lint': lint_requires,
+    entry_points={
+        "console_scripts": [
+            "{{cookiecutter.cli_name}} = {{cookiecutter.package_name}}.cli:main"
+        ]
     },
-    cmdclass={'test': PyTest})
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    extras_require={"test": tests_requires, "dev": dev_requires, "lint": lint_requires},
+    cmdclass={"test": PyTest},
+)
--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cli.py
@@ -3,4 +3,4 @@ import click
 
 @click.command()
 def main():
-    print('I am {{cookiecutter.cli_name}}')
+    print("I am {{cookiecutter.cli_name}}")
